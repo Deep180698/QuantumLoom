9aa23859c7fbae992b65491d2407fcc425728796
Deep <pateldeep0989@gmail.com>
2024-12-18 18:19:03 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-500">Hello, Tailwind!</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import ThreeScene from './ThreeScene';
+
+function App() {
+  return (
+    <div className="h-screen w-screen bg-gray-900">
+      <ThreeScene />
+    </div>
+  );
+}
+
+export default App;
